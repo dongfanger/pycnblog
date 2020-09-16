6cc17049815d67c1c4012c7a8fe0c2557173b77c
王刚 <dongfang@class100.com>
2020-09-16 12:46:53 +0800

    支持图片转为<img>标签；图片正则改为贪婪模式；

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ username = 'xxx'
 password = 'xxx'
 gen_network_file = 0  # 是否生成图片替换后文件,默认关闭
 publish = False  # 上传后是否发布，默认未发布，设置True为发布
-img_tag = False  # 图片上传后转为<img>标签，设置width
+img_tag = False  # 图片上传后转为<img>标签，设置width，居中
+img_width = "100%"  # 图片宽度，默认100%，结合img_tag使用
